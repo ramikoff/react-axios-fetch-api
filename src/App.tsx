9e940465cee2da223603756b6e978951d165485b
Ramil Novruzov <ramil.novruzov@adesso.de>
2022-12-15 11:09:07 +0100

    axios fetch api added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {Product} from "./components/Product";
+import axios from "axios";
 
 function App() {
+
+
+  async function getProducts() {
+    const res = await axios.get('https://fakestoreapi.com/products')
+    console.log(res.data)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
-    <div className="text-3xl font-bold underline">
+    <div className="container mx-auto max-w-2xl pt-5">
     <Product />
     </div>
   );
